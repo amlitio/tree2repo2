65df2a20ed7a2af057187bfb8693c4bb85d52d0d
ubu env <amlitllc@gmail.com>
2026-03-06 05:32:42 -0800

    fixes coddez app js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import { Inter, Roboto_Mono } from 'next/font/google';
 import '../styles/globals.css';
 
+// Load Aave's official brand fonts
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+const robotoMono = Roboto_Mono({
+  subsets: ['latin'],
+  variable: '--font-roboto-mono',
+  display: 'swap',
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} ${robotoMono.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
